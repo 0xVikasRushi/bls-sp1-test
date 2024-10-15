6def032a22691fe10f19b1c63a62cfa7da87cdb2
feltroidprime <feltroidprime@protonmail.com>
2024-10-15 12:42:33 +0700

    rm

--- lib/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use alloy_sol_types::sol;
-
-sol! {
-    /// The public values encoded as a struct that can be easily deserialized inside Solidity.
-    struct PublicValuesStruct {
-        uint32 result;
-    }
-}
